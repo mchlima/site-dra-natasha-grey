097a77499d593d403d671b814835140c264824e2
Michel Lima <m.macedomarques@gmail.com>
2025-08-11 11:06:09 -0300

    feat: SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/scripts']
-})
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/scripts"],
+  app: {
+    head: {
+      title: "Dra. Natasha Grey - Odontologia",
+      meta: [
+        { name: "description", content: "Clínica Odontológica em São Paulo" },
+      ],
+    },
+  },
+});
