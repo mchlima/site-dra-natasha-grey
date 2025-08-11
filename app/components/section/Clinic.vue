f19bd739d2025ea3df973dfa4b2c080a041ed7c8
Michel Lima <m.macedomarques@gmail.com>
2025-08-11 18:22:16 -0300

    refact: refact: clinic section

--- app/components/section/Clinic.vue
+++ app/components/section/Clinic.vue
@@ -3,12 +3,41 @@
   <section id="clinic" class="clinic container-fluid">
     <div class="container">
       <h2>Nossa Clínica, Seu Sorriso</h2>
-      <p>Somos uma Clínica Odontológica Multidisciplinar em São Paulo com mais de 20 anos de experiência. Nosso
-        compromisso é trabalhar harmoniosamente com o seu sorriso e rosto, objetivando a reabilitação orofacial em
-        sua totalidade, ou seja, promovendo saúde, função e estética de forma satisfatória. Para isso, contamos com
-        uma equipe especializada, capacitada e experiente nas mais diversas áreas da prática odontológica.</p>
+
+
+
+
       <div class="container">
-        <div class="clinic-card">
+
+        <p>
+          Somos uma Clínica Odontológica Multidisciplinar em São Paulo com mais de 20 anos de experiência. Nosso
+          compromisso é trabalhar harmoniosamente com o seu sorriso e rosto, objetivando a reabilitação orofacial em
+          sua totalidade, ou seja, promovendo saúde, função e estética de forma satisfatória. Para isso, contamos com
+          uma equipe especializada, capacitada e experiente nas mais diversas áreas da prática odontológica.
+        </p>
+
+        <div>
+          <ClientOnly>
+            <swiper-container ref="containerRef">
+              <swiper-slide>
+                <NuxtImg src="/images/clinica1.jpg" alt="recepção" />
+              </swiper-slide>
+
+              <swiper-slide>
+                <NuxtImg src="/images/clinica3.jpg" alt="sala espera" />
+              </swiper-slide>
+
+              <swiper-slide>
+                <NuxtImg src="/images/clinica2.jpg" alt="consultório" />
+              </swiper-slide>
+            </swiper-container>
+          </ClientOnly>
+        </div>
+
+
+
+
+        <!-- <div class="clinic-card">
           <h3>Recepção</h3>
           <NuxtImg src="/images/clinica1.webp" alt="recepção" width="380" />
           <h4>Um sorriso para receber você</h4>
@@ -27,9 +56,38 @@
           <NuxtImg src="/images/clinica2.webp" alt="consultório" width="380" />
           <h4>Cuidado e atenção nos detalhes</h4>
           <p>Espaço moderno e equipado para garantir um atendimento seguro e de qualidade.</p>
-        </div>
+        </div> -->
+      </div>
+
+      <div>
+        <button @click="swiper.prev()">
+          Prev
+        </button>
+
+        <button @click="swiper.next()">
+          Next
+        </button>
       </div>
     </div>
   </section>
   <!-- /section clinic -->
 </template>
+
+<script setup lang="ts">
+// Create 10 slides
+const containerRef = ref(null)
+const swiper = useSwiper(containerRef)
+
+onMounted(() => {
+  console.log(swiper.instance)
+})
+</script>
+
+<style lang="css">
+swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxt/scripts",
     "@nuxtjs/google-fonts",
+    "nuxt-swiper",
   ],
 
   css: [
