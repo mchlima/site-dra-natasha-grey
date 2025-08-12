7a6f3e07a050193b7c20514f02eef76660ab6bc0
Michel Lima <m.macedomarques@gmail.com>
2025-08-11 23:36:00 -0300

    feat: hero

--- app/components/section/Hero.vue
+++ app/components/section/Hero.vue
@@ -3,8 +3,15 @@
   <section id="hero" class="hero container-fluid">
 
     <NuxtImg src="/images/hero-bg.webp" fetchpriority="high" alt="Dra. Natasha Grey" />
-    <div class="hero-overlay">
-      a
+    <div class="hero-overlay"></div>
+    <div class="container">
+      <div class="hero-content">
+        <h2>Cuidado que Transforma</h2>
+        <p>Na clínica da Dra. Natasha, cada paciente é único. Aqui, você encontra mais do que tratamentos: encontra
+          atenção, acolhimento e resultados que refletem o seu melhor.</p>
+        <span>Redescubra sua melhor versão</span>
+        <button>agende agora</button>
+      </div>
     </div>
   </section>
   <!-- /hero -->
